bfa97eabab9782e83a508f14f68a9239b43a0c7e
Will Gibson <wgibson75@hotmail.com>
2023-05-06 16:13:54 +0100

    Include seconds in file date for directory listings.

--- webserver/server.js
+++ webserver/server.js
@@ -339,7 +339,7 @@ function createSnapshotSummary(timestamp, images) {
 
 function getDateString(d) {
     return d.getFullYear() + '-' + ('0' + (d.getMonth() + 1)).slice(-2) + '-' + ('0' + d.getDate()).slice(-2)
-        + ' ' + ('0' + d.getHours()).slice(-2) + ':' + ('0' + d.getMinutes()).slice(-2);
+        + ' ' + ('0' + d.getHours()).slice(-2) + ':' + ('0' + d.getMinutes()).slice(-2) + ':' + ('0' + d.getSeconds()).slice(-2);
 }
 
 function getSizeString(s) {
