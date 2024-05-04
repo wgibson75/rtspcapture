ec193c6d58465c60a756aee62d2ae9839ecd0f27
Will Gibson <wgibson75@hotmail.com>
2024-05-04 08:47:14 +0000

    Only add the initial default user account if no users currently exist

--- docker/webserver/app/db.js
+++ docker/webserver/app/db.js
@@ -17,14 +17,18 @@ db.serialize(function() {
     hashed_password BLOB, \
     salt BLOB \
   )");
-  
-  // Create the initial default user
-  var salt = crypto.randomBytes(16);
-  db.run('INSERT OR IGNORE INTO users (username, hashed_password, salt) VALUES (?, ?, ?)', [
-    DEFAULT_USERNAME,
-    crypto.pbkdf2Sync(DEFAULT_PASSWORD, salt, 310000, 32, 'sha256'),
-    salt
-  ]);
+
+  db.get('SELECT COUNT(*) AS count FROM users', (err, row) => {
+    // If no users, create the initial default user
+    if (row.count == 0) {
+      var salt = crypto.randomBytes(16);
+      db.run('INSERT OR IGNORE INTO users (username, hashed_password, salt) VALUES (?, ?, ?)', [
+        DEFAULT_USERNAME,
+        crypto.pbkdf2Sync(DEFAULT_PASSWORD, salt, 310000, 32, 'sha256'),
+        salt
+      ]);
+    }
+  });
 });
 
 module.exports = db;
