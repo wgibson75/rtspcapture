f1b0f32ee92e4e896d99aa2a813edd52e3225077
Will Gibson <wgibson75@hotmail.com>
2026-03-26 10:07:14 +0000

    Move configuration loading and logging setup for capture into a utility module

--- docker/capture/app/capture.py
+++ docker/capture/app/capture.py
@@ -11,11 +11,12 @@ import glob
 import shutil
 import json
 import logging
-from logging.handlers import RotatingFileHandler
 from types import SimpleNamespace
 from datetime import datetime,timezone
 from abc import ABC, abstractmethod
 from onvif import ONVIFCamera
+
+import utils
 
 logger = logging.getLogger(__name__)
 onvif_wsdl_defs = None
@@ -318,49 +319,9 @@ class CheckDiskUsage:
         logger.info(f'#### Deleting oldest recording: {os.path.basename(files[0])}')
         os.remove(files[0])
 
-def load_config(config_file):
-    # Convert dictionary items to attributes
-    class AttrifyDict(dict):
-
-        def __init__(self, value):
-            if isinstance(value, dict):
-                for key in value:
-                    self.__setitem__(key, value[key])
-
-        def __setitem__(self, key, value):
-            if isinstance(value, dict) and not isinstance(value, AttrifyDict):
-                value = AttrifyDict(value)
-            elif isinstance(value, list): # Support objects in a list
-                value = [AttrifyDict(item) for item in value]
-            super().__setitem__(key, value)
-
-        def __getitem__(self, key):
-            return self.get(key)
-
-        __setattr__, __getattr__ = __setitem__, __getitem__
-
-    with open(config_file) as json_config:
-        config = json.load(json_config)
-    return AttrifyDict(config)
-
 def health_check(cc_list):
     for cc in cc_list:
         cc.health_check()
-
-def configure_logging(config):
-    logger.setLevel(logging.DEBUG)
-
-    log_file = os.path.join(config.root_path, config.logs_dir, config.capture_log)
-    os.makedirs(os.path.dirname(log_file), 0o777, True)
-
-    handler = RotatingFileHandler(
-        log_file,
-        maxBytes=config.log_max_bytes,
-        backupCount=config.log_backup_count
-    )
-    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
 
 def capture_from_cameras(config):
     global update_dead_time_secs
@@ -424,8 +385,10 @@ def main():
     parser.add_argument('config_file', help='CCTV JSON configuration file.')
     args = parser.parse_args()
 
-    config = load_config(args.config_file)
-    configure_logging(config)
+    config = utils.load_config(args.config_file)
+
+    log_file = os.path.join(config.root_path, config.logs_dir, config.capture_log)
+    utils.configure_logging(logger, log_file, config.log_max_bytes, config.log_backup_count)
 
     try:
         capture_from_cameras(config)
---
+++ docker/capture/app/utils.py
@@ -0,0 +1,44 @@
+import json
+import logging
+from logging.handlers import RotatingFileHandler
+import os
+
+def load_config(config_file):
+    # Convert dictionary items to attributes
+    class AttrifyDict(dict):
+
+        def __init__(self, value):
+            if isinstance(value, dict):
+                for key in value:
+                    self.__setitem__(key, value[key])
+
+        def __setitem__(self, key, value):
+            if isinstance(value, dict) and not isinstance(value, AttrifyDict):
+                value = AttrifyDict(value)
+            elif isinstance(value, list): # Support objects in a list
+                value = [AttrifyDict(item) for item in value]
+            super().__setitem__(key, value)
+
+        def __getitem__(self, key):
+            return self.get(key)
+
+        __setattr__, __getattr__ = __setitem__, __getitem__
+
+    with open(config_file) as json_config:
+        config = json.load(json_config)
+    return AttrifyDict(config)
+
+def configure_logging(logger, log_file, max_bytes, backup_count):
+    logger.setLevel(logging.DEBUG)
+
+    os.makedirs(os.path.dirname(log_file), 0o777, True)
+
+    handler = RotatingFileHandler(
+        log_file,
+        maxBytes=max_bytes,
+        backupCount=backup_count
+    )
+    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
