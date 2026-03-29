a2d299213a0cca938a29c0d71777451a1bfc7c9b
Will Gibson <wgibson75@hotmail.com>
2026-03-29 23:45:59 +0100

    Improved config handling for lists of non dictionary elements

--- docker/capture/app/config.py
+++ docker/capture/app/config.py
@@ -13,9 +13,8 @@ def load(config_file):
         def __setitem__(self, key, value):
             if isinstance(value, dict) and not isinstance(value, AttrifyDict):
                 value = AttrifyDict(value)
-            elif isinstance(value, list):  # Support objects in a list
-                if not isinstance(value[0], str):  # But support lists of strings as is
-                    value = [AttrifyDict(item) for item in value]
+            elif isinstance(value, list) and isinstance(value[0], dict):  # Support objects in a list
+                value = [AttrifyDict(item) for item in value]
             super().__setitem__(key, value)
 
         def __getitem__(self, key):
